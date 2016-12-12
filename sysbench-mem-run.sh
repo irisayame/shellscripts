47669266e1fd8a4ac0c588ec7e802d8c4b35b576
ubuntu <ubuntu@8vcpu-64gb-01.novalocal>
2016-12-12 06:33:50 +0000

    :pencil2: log name format

--- sysbench-mem-run.sh
+++ sysbench-mem-run.sh
@@ -3,21 +3,21 @@
 MEMORY_TOTAL_SIZE=$1
 MEMORY_BLOCK_SIZE=$2
 
-if [ "$MEMORY_TOTAL_SIZE" == "" ];
+if [ "$MEMORY_TOTAL_SIZE" == "" ] ;then
      echo "NEED input MEMORY_TOTAL_SIZE"
-     exit 0
+     MEMORY_TOTAL_SIZE=10G
 fi
 
-if [ "$MEMORY_BLOCK_SIZE" == "" ];
+if [ "$MEMORY_BLOCK_SIZE" == "" ] ;then
      echo "NEED input MEMORY_BLOCK_SIZE"
-     exit 0
+     MEMORY_BLOCK_SIZE=1G
 fi
 
-dstat --time --cpu --sys --load --proc --top-cpu --mem | tee dstat-`date "+%Y%m%d%H%M"`.log &
+dstat --time --cpu --sys --load --proc --top-cpu --mem | tee dstat-memory-$MEMORY_TOTAL_SIZE-$MEMORY_BLOCK_SIZE-`date "+%Y%m%d%H%M"`.log &
 sleep 3
 
-echo "sysbench --test=memory --memory-total-size=$MEMORY_TOTAL_SIZE --memory-block-size=$MEMORY_BLOCK_SIZE run | tee sysbench-`date +%Y%m%d%H%M`-memory-$MEMORY_TOTAL_SIZE-$MEMORY_BLOCK_SIZE.log" >> run-sysbench.log
-sysbench --test=memory --memory-total-size=400G --memory-block-size=4G run 2>&1 > "sysbench-`date "+%Y%m%d%H%M"`-memory.log" 
+echo "sysbench --test=memory --memory-total-size=$MEMORY_TOTAL_SIZE --memory-block-size=$MEMORY_BLOCK_SIZE run | tee sysbench-memory-$MEMORY_TOTAL_SIZE-$MEMORY_BLOCK_SIZE-`date +%Y%m%d%H%M`.log" >> run-sysbench.log
+sysbench --test=memory --memory-total-size=$MEMORY_TOTAL_SIZE --memory-block-size=$MEMORY_BLOCK_SIZE run 2>&1 > "sysbench-memory-$MEMORY_TOTAL_SIZE-$MEMORY_BLOCK_SIZE-`date "+%Y%m%d%H%M"`-memory.log" 
 sleep 5
 
 pid=`ps aux | grep dstat | awk -F" " '{print $2}'`
